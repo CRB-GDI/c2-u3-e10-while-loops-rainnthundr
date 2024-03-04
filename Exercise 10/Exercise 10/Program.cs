a834d6b2b0a18c91b365251be48af3e8035fd219
rainnthundr <145371191+rainnthundr@users.noreply.github.com>
2024-03-04 13:17:23 -0500

    Update Program.cs

--- Exercise 10/Exercise 10/Program.cs
+++ Exercise 10/Exercise 10/Program.cs
@@ -12,6 +12,26 @@
             Console.WriteLine("4 - Quit");
             Console.WriteLine("**************");
             int gamePlay = int.Parse(Console.ReadLine());
+
+            for (int g = 1; g <= 4; g++) ;
+            {
+                if (gamePlay == 1) ;
+                {
+                    Console.WriteLine("Loading Game...");
+                    for (int d = 0; 0 < 4; d++) ;
+   
+                }
+                    
+            }
+            for (int g = 2; g <= 4; g++) ;
+            {
+                 if (gamePlay == 2) ;
+             {
+                 Console.Write("Select which game you wish to play: ");
+                 for (int l = 0; 0 < 4; l++) ;
+                  int p = int.Parse(Console.ReadLine());
+             }
+            }
         }
     }
 }
